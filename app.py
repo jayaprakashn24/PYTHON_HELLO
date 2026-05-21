8ed4a886c4f31a24faba917784d5110dad1ff083
Jayaprakash N <jayaprakashn900@gmail.com>
2026-05-21 13:50:52 +0530

    code updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "we are learning jaya_praksh"
+    return "we are learning aws and devopps jaya_praksh"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
